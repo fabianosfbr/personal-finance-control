5f735fced91b3e9fc76fe9e07f926830417d8d20
Fabiano Fernandes <contato@fabianofernandes.adm.br>
2022-09-15 12:39:06 -0300

    Init

---
+++ app/Models/Feature.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Feature extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'type',
+        'name',
+        'slug',
+    ];
+
+    public function plan(){
+        return $this->belongsTo(Plan:class);
+    }
+}
---
+++ app/Models/UserPlan.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserPlan extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'plan_id',
+        'use_id',
+        'reference_transation'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User:class);
+    }
+
+    public function plan(){
+        return $this->belongsTo(Plan:class);
+    }
+
+
+}
